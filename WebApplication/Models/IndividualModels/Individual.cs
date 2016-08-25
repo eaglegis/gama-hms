99aa3aba463a8344a24090ba2d3a9f6e01da4689
Leigh Hunt <leigh@venari.co.nz>
2016-08-25 16:29:55 +1200

    Model tweak before ef migration

--- WebApplication/Models/IndividualModels/Individual.cs
+++ WebApplication/Models/IndividualModels/Individual.cs
@@ -16,31 +16,37 @@ namespace WebApplication.Models
       public string Tribe { get; set; }
       public string Clan { get; set; }
       public string Village { get; set; }
+
+      public Organisation Organisation { get; set; }
   }
 
   public class Appointment
   {
     public int Id { get;set; }
     public Individual Individual { get; set; }
-    public Organisation Organisation { get; set; }
-    public Role Role { get; set; }
+    public Department Department { get; set; }
+    public Position Position { get; set; }
 
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-
   }
 
   public class Organisation
   {
     public int Id { get; set; }
     public string Name { get; set; }
-
   }
 
-  public class Role
+  public class Department
   {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+  }
+
+  public class Position
+  {
+    public int Id { get;set; }
+    public string Title { get; set; }
+    public string Decsription { get; set; }
   }
 }
