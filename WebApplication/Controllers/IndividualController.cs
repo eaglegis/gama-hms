5119cdca4344f505892a4521bcfbb75443d897e8
Leigh Hunt <leigh@venari.co.nz>
2016-09-05 12:47:54 +1200

    Simple Individual CRUD operations

--- WebApplication/Controllers/IndividualController.cs
+++ WebApplication/Controllers/IndividualController.cs
@@ -14,26 +14,51 @@ namespace WebApplication.Controllers
 
     public class IndividualController : Controller
     {
-      private ApplicationDbContext _context;
+        private ApplicationDbContext _context;
 
-      public IndividualController(ApplicationDbContext context)
-      {
-          _context = context;
-      }
+        public IndividualController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-      // GET api/individual
-      [HttpGet]
-      public IEnumerable<Individual> Index()
-      {
-          return _context.Individuals.ToList();
-      }
+        // GET api/individual
+        [HttpGet]
+        public IEnumerable<Individual> Index()
+        {
+            return _context.Individuals.ToList();
+        }
 
-      // GET api/individual
-      [HttpGet("{id}")]
-      public Individual Get(int id)
-      {
-        return _context.Individuals.Where<Individual>(I => I.Id == id).Single();
-      }
+        // GET api/individual
+        [HttpGet("{id}")]
+        public Individual Get(int id)
+        {
+            return _context.Individuals.Where<Individual>(I => I.Id == id).Single();
+        }
 
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Individual individual)
+        {
+            _context.Individuals.Add(individual);
+            _context.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Individual individual)
+        {
+            _context.Individuals.Update(individual);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Individual individual = _context.Individuals.First(i => i.Id == id);
+            _context.Individuals.Remove(individual);
+            _context.SaveChanges();
+        }
     }
 }
