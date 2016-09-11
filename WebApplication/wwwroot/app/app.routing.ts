86810cef63c5244c2b3b33c601d031526677666c
Leigh Hunt <leigh@venari.co.nz>
2016-09-12 11:05:13 +1200

    In progress - still not working

--- WebApplication/wwwroot/app/app.routing.ts
+++ WebApplication/wwwroot/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent }     from './dashboard.component';
 import { IndividualsComponent }     from './individuals.component';
 import { IndividualDetailComponent } from './individual-detail.component';
 
@@ -9,6 +10,10 @@ const appRoutes: Routes = [
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   {
     path: 'detail/:id',
---
+++ WebApplication/wwwroot/app/dashboard.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { Individual }        from './individual';
+import { IndividualService } from './individual.service';
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  individuals: Individual[] = [];
+
+  constructor(
+    private router: Router,
+    private individualService: IndividualService) {
+  }
+
+  ngOnInit(): void {
+    this.individualService.getIndividuals()
+      //.then(individuals => this.individuals = individuals.slice(1, 5));
+      .then(individuals => console.log("Individuals: " + this.individuals.length))
+  }
+
+  gotoDetail(individual: Individual): void {
+    let link = ['/detail', individual.id];
+    this.router.navigate(link);
+  }
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
