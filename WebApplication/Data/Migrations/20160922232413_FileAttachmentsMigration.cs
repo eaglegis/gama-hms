9f2b60c44b0eb0954ddc7a4e5d075c280f059ee4
Leigh Hunt <leigh@venari.co.nz>
2016-09-23 11:27:43 +1200

    dotnet ef migrations add FileAttachmentsMigration; dotnet ef database update

---
+++ WebApplication/Data/Migrations/20160922232413_FileAttachmentsMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Data.Migrations
+{
+    public partial class FileAttachmentsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileAttachments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    FileContentsBase64 = table.Column<string>(nullable: true),
+                    Filename = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileAttachments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileAttachments");
+        }
+    }
+}
