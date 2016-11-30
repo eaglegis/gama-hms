e08bede0e6e67fea688939e21a01398e162cb7ab
Leigh Hunt <leigh@venari.co.nz>
2016-11-30 13:40:37 +1000

    Changing port 5000 -> 50001

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,7 @@ namespace WebApplication
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://*:5000/")
+                .UseUrls("http://*:5001/")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
